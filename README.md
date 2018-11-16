ab8148c17394d28019fc9ba7b8b6cde17238aa5d
ang00 <zhang10092009@hotamicl.com>
2018-11-16 16:55:17 +0800

    int fastDFS

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # fastDfsServer
 fastDFS 文件服务器
+
+#### 文件服务器搭建部署
+
+> 参考(http://www.ityouknow.com/springboot/2018/01/16/spring-boot-fastdfs.html)
+
+#### `docker` 部署 `springboot`  项目 
+
+> 参考(http://www.cnblogs.com/silentdoer/p/8975577.html)
+
--- fastDfsServer/src/main/java/com/forteach/fastdf/server/controller/UploadTestController.java
+++ fastDfsServer/src/main/java/com/forteach/fastdf/server/controller/UploadTestController.java
@@ -1,59 +1,36 @@
 package com.forteach.fastdf.server.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.forteach.fastdf.server.fastdfs.FastDFSClient;
 import com.forteach.fastdf.server.fastdfs.FastDFSFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @Auther: zhangyy
  * @Email: zhang10092009@hotmail.com
- * @Date: 18-11-14 14:22
+ * @Date: 18-11-16 12:51
  * @Version: 1.0
  * @Description:
  */
 @Controller
-@ResponseBody
-@RequestMapping("/")
-public class UploadController {
-    private static Logger logger = LoggerFactory.getLogger(UploadController.class);
+@RequestMapping("/test")
+public class UploadTestController {
 
-    @PostMapping("/upload")
-    public String upload(@RequestParam("file") MultipartFile file, HttpServletRequest request, HttpServletResponse response){
-        if (file.isEmpty()) {
-//            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
-            return "文件不存在请重新上传！";
-//            return "redirect:uploadStatus";
-        }
-        try {
-            // Get the file and save it somewhere
-            String path=saveFile(file);
-            Map map = new HashMap<String, String>();
-            map.put("fileName", file.getOriginalFilename());
-            map.put("filePath", path);
-            String jsonString = JSONObject.toJSONString(map);
-            return jsonString;
-        } catch (Exception e) {
-            logger.error("upload file failed",e);
-        }
-        return null;
-    }
+    private static Logger logger = LoggerFactory.getLogger(UploadTestController.class);
 
     @GetMapping("/")
     public String index() {
-        return "redirect:uploadHtml";
+        return "uploadHtml";
     }
 
     @PostMapping("/uploadHtml") //new annotation since 4.3
@@ -88,7 +65,7 @@ public class UploadController {
      * @return
      * @throws IOException
      */
-    public String saveFile(MultipartFile multipartFile) throws IOException {
+    private String saveFile(MultipartFile multipartFile) throws IOException {
         String[] fileAbsolutePath={};
         String fileName=multipartFile.getOriginalFilename();
         String ext = fileName.substring(fileName.lastIndexOf(".") + 1);
